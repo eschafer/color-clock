c1568e474f9ce4d5a558b6bee158420bbfba0b02
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2013-04-13 12:32:52 -0400

    Spaces to tabs

--- karma.conf.js
+++ karma.conf.js
@@ -5,14 +5,14 @@ basePath = '';
 
 // list of files / patterns to load in the browser
 files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/components/angular/angular.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
+	JASMINE,
+	JASMINE_ADAPTER,
+	'app/components/angular/angular.js',
+	'app/components/angular-mocks/angular-mocks.js',
+	'app/scripts/*.js',
+	'app/scripts/**/*.js',
+	'test/mock/**/*.js',
+	'test/spec/**/*.js'
 ];
 
 // list of files to exclude
