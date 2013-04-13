10653112c807274db29792d816a5d4839b78d5f6
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2013-04-13 13:23:59 -0400

    Added basic clock

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,11 @@
 "use strict";
 
-angular.module("colorClockApp")
-	.controller("MainCtrl", function ($scope) {
-		$scope.awesomeThings = [
-			"HTML5 Boilerplate",
-			"AngularJS",
-			"Karma"
-		];
+angular.module("colorClockApp").controller("MainCtrl", function ($scope, $timeout) {
+	$scope.date = new Date();
+
+	$scope.$watch("date", function(){
+		$timeout(function(){
+			$scope.date = new Date();
+		}, 1000);
 	});
+});
